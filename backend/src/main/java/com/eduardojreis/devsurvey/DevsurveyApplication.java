838c72078c6c35807ed1c325441684d92d41dbda
Eduardo-JReis <edu.publicidade81@gmail.com>
2021-04-17 21:48:37 -0300

    Project created

---
+++ backend/src/main/java/com/eduardojreis/devsurvey/DevsurveyApplication.java
@@ -0,0 +1,13 @@
+package com.eduardojreis.devsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsurveyApplication.class, args);
+	}
+
+}
